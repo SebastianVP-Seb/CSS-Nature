615c3fa55d52d7ea52fade1f43fad7596ee0353c
SebastianVP-Seb <palacios.sebastian.24@outlook.com>
2021-07-03 19:35:23 -0500

    Finalizando el Header e incluyendo animaciones al H1

---
+++ apuntes.js
@@ -0,0 +1,45 @@
+/*           PARA EL CSS
+
+* { Este es el selector universal, por defecto, los navegadores agregan un margen y padding
+    a los sitios web
+    box-sizing: border-box; hace que los bordes y los paddings no se agreguen a las cajas que se 
+        especifican para una caja
+}
+
+body {
+    Aquí siempre se definen las fuentes, no en el selector universal
+}
+
+height: 95vh; La altura ocupará el 95% de la pantalla. Las imágenes cambian dinámicamente al cambiar
+de tamaño la pantalla
+background-size: cover; arregla la img para que se vea toda en el contenedor
+
+.clase {
+
+}
+
+///////////// ANIMACIONES ///////////////
+Se crea una animación con @keyframes nombreDeLaAnimación { dentro se especifica el avance en % de 
+    la animación } Para que ésta se vea bien, es conveniente poner sólo dos animaciones: 
+    opacity y transform. 
+Luego, se manda a llamar la animación dentro del elemento que se quiere animar y se establece duración
+
+animation-delay: 2s; Tiempo para que inicie una animación
+animation-iteration-count: 2; # de veces que se ejecutará la animación
+animation-timing-function: ease-in; establece la velocidad de la animación, puede empezar rápido y luego
+    lento o viceversa
+animation-timing-function: cubic-bezier(1, 0, 0, 1); nuestra f personalizada
+75% {
+        transform: rotate(50deg);        Rotación
+    }
+
+
+
+
+
+
+
+
+
+
+*/
